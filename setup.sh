033481ade2bdb3eaf2e0ed6761f29c3c5b48b0f4
tomsato <tmhr.st9711@gmail.com>
2016-04-10 12:09:03 +0900

    add bash_profile

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# .vimrc
 if [ ! -f ~/.vimrc ]; then
     ln -s `dirname $0`/_vimrc ~/.vimrc
 fi
 
+# .bash_profile
+if [ ! -f ~/.bash_profile ]; then
+    ln -s `dirname $0`/_bash_profile ~/.bash_profile
+fi
+
+# git
+git config --global color.ui auto
+git config --global alias.co "checkout"
+git config --global alias.st "status"
+git config --global alias.br "branch"
+git config --global alias.ci "commit"
 
